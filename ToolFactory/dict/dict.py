98eef9c2056a8aa3ef4aaa520829f5697007e398
ExBorn <992122833@qq.com>
2017-02-12 22:06:39 +0800

    update: daily

--- ToolFactory/dict/dict.py
+++ ToolFactory/dict/dict.py
@@ -4,11 +4,13 @@ import re
 import sys
 import requests
 import sys
+import urllib.parse
 
 class Dict:
 
     def __init__(self, word):
-        self.word = word
+        self.word = urllib.parse.quote(word)
+        print(self.word)
         self.result = None
         self.url = None
 
@@ -31,13 +33,20 @@ class Dict:
 
     def getResult(self):
         pattern = re.compile('<span class='+'"'+'prop'+'"'+'>.*?</span>.*?<p>(.*?)</p>',re.S)
-        result = re.search(pattern, self.getPage())
+        page = self.getPage()
+        result = re.search(pattern, page)
         if result:
         #print result.group(1)  #测试输出
             self.result = result.group(1).strip().replace('<span>', '').replace('</span>', '').replace(' ', '').replace('；', '')
-            return self.result
-        else:
-            return None
+        # else:
+        elif None:
+            # TODO: 翻译句子
+            # TODO: 翻译中文单词
+            # TODO: 翻译中文句子
+            pattren = re.compile(r'<div style="width: 580px; margin-top: 15px; font-size: 15px; line-height: 24px; color: #333333;">(.+)</div>')
+            result = re.search(pattren, page)
+            self.result = result.group(1)
+        return self.result
 
     def run(self):
         return self.getResult()
@@ -59,5 +68,9 @@ def main():
         print(helps)
 
 
+def test():
+    print(Dict('The official home of the Python Programming Language.').icibaGetPage())
+
 if __name__ == '__main__':
     main()
+    # test()
