a3dd030d3f4e2ac8f1f6954f096af2802a478af2
WinNB1\ExFly <exfly@outlook.com>
2018-04-20 11:36:09 +0800

    update 增加一些java常用代码

---
+++ Language/Java/code/READEM.md
@@ -0,0 +1,3 @@
+* [20个非常有用的Java程序片段 -- 陈皓](https://mp.weixin.qq.com/s/KFOdrEbhXcsUzQq-g9ZLqQ)
+
+* mybatis spring 使用例子 http://mybatis.tk/
---
+++ Language/Java/code/util/ArrayUtil.java
@@ -0,0 +1,14 @@
+package org.exfly.smart.framework.util;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+public class ArrayUtil {
+
+    public static boolean isNotEmpty(Object[] array) {
+        return !ArrayUtils.isEmpty(array);
+    }
+
+    public static boolean isEmpty(Object[] array) {
+        return ArrayUtils.isEmpty(array);
+    }
+}
